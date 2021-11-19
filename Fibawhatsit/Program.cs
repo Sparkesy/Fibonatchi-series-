ccf2a1e8c8b69d7bffd0be4a4d8fe9891c7350d9
Jordan Sparkes <Jordan.sparkes@jsmsdn.onmicrosoft.com>
2021-11-19 11:47:16 +0000

    Add project files.

---
+++ Fibawhatsit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fibawhatsit {
+    class Program {
+        static void Main (string[] args) {
+            Console.Write("Please enter the Whole number : ");
+            int number = int.Parse(Console.ReadLine());
+            Fib(0, 1, 1, number);
+            Console.ReadKey();
+        }
+        static void Fib (int firstNumber, int secondNumber, int counter, int number) {
+            Console.Write(firstNumber + " ");
+            if (counter < number) {
+                Fib(secondNumber, firstNumber + secondNumber, counter + 10, number);
+            }
+        }
+    }
+}
+
